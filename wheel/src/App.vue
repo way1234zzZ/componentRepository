4b7bcdc64fea594f1f3ed2b78cc51b5624df9654
way1234zzZ <2979523100@qq.com>
2020-12-01 17:30:52 +0800

    button组件与icon组件

---
+++ wheel/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <g-button>
+      按钮
+    </g-button>
+    <g-button icon="settings">
+      按钮
+    </g-button>
+    <g-button icon="download" iconPosition="right">
+      按钮
+    </g-button>
+    <g-icon icon="download"></g-icon>
+  </div>
+</template>
+
+<script>
+import gButton from '@/components/button.vue'
+import gIcon from '@/components/icon.vue'
+export default {
+  components: {
+    gButton,
+    gIcon
+  },
+  data() {
+    return {
+      // iconName: 'settings'
+    }
+  }
+}
+</script>
+<style scoped>
+#app {
+  margin: 20px;
+}
+</style>
---
+++ wheel/src/components/icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${icon}`"></use>
+  </svg>
+</template>
+<script>
+export default {
+  name: 'gIcon',
+  props: {
+    icon: {
+      default: '',
+      type: String
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.g-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
