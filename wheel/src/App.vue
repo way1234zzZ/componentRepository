bd7b5ca4b5a849506a5ce83c4f1e0d8ed5a13280
way1234zzZ <2979523100@qq.com>
2020-12-03 20:51:04 +0800

    完成button-group

--- wheel/src/App.vue
+++ wheel/src/App.vue
@@ -8,15 +8,28 @@
     </g-button>
     <g-button :loading="flag" icon="download" iconPosition="right" @click="flag = !flag">
       按钮
-    </g-button>
+    </g-button> |
+    <button-group>
+      <g-button icon="left">
+        上一页
+      </g-button>
+      <g-button>
+        更多
+      </g-button>
+      <g-button icon="right" iconPosition="right">
+        下一页
+      </g-button>
+    </button-group>
   </div>
 </template>
 
 <script>
 import gButton from '@/components/button.vue'
+import buttonGroup from '@/components/button-group.vue'
 export default {
   components: {
     gButton,
+    buttonGroup
   },
   data() {
     return {
