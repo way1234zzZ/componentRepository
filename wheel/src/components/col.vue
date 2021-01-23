1069b427d22050afbf5b84fe46fffbab99e58f68
way1234zzZ <2979523100@qq.com>
2021-01-23 23:25:13 +0800

    row,col的重构

--- wheel/src/components/col.vue
+++ wheel/src/components/col.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- 如果有offset -->
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]" :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
-    <div style="border:1px solid green;height:100px">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClasses" :style="colStyle">
+    <!-- <div style="border:1px solid green;height:100px"> -->
+    <slot></slot>
+    <!-- </div> -->
   </div>
 </template>
 <script>
@@ -11,7 +11,26 @@ export default {
   name: 'gCol',
   data() {
     return {
-      gutter: 0
+      gutter: 0,
+
+    }
+  },
+  computed: {
+    colClasses() {
+      //解构，快速获取this对象中的span offset
+      let { span, offset } = this
+      return [
+        span && `col-${span}`,
+        offset && `offset-${offset}`
+      ]
+      //return [this.span && `col-${this.span}`, this.offset && `offset-${this.offset}`]
+
+    },
+    colStyle() {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
+      }
     }
   },
   props: {
