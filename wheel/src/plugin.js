083c2721db77fbab03ce363bece71ea55453013b
way1234zzZ <2979523100@qq.com>
2021-01-30 08:41:33 +0800

    解决多次点击toast出现多个div的问题

--- wheel/src/plugin.js
+++ wheel/src/plugin.js
@@ -1,25 +1,25 @@
 import Toast from './components/toast'
+
+let currentToast
+
 export default {
   install(Vue) {
     Vue.prototype.$toast = function (message, toastOptions) {
-      // console.log('I am a toast')
-      // alert(message)
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData:
-          //放到tools.vue里调用传了
-          // closeButton: {
-          //   text: '知道了',
-          //   callBack: () => {
-          //     console.log('用户说他知道了')
-          //   }
-          // }
-          toastOptions
-
-      })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+      //函数里的对象扩展 message:message然后简写
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({ Vue, message, propsData: toastOptions })
     }
   }
+}
+
+//js会自动把函数提到上面
+function createToast({ Vue, message, propsData }) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({ propsData })
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
 }
