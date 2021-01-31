cc738af2b3b92eae030d12d74f13fa3c787a482f
way1234zzZ <2979523100@qq.com>
2021-01-31 23:39:12 +0800

    完善tabs

--- wheel/src/components/tabs.vue
+++ wheel/src/components/tabs.vue
@@ -32,6 +32,10 @@ export default {
   },
   mounted() {
     this.$children.forEach((vm) => {
+      if (this.$children.length === 0) {
+        //如果console console.warn存在 有的浏览器不存在
+        console && console.warn && console.warn('tabs的子组件应该是tabs-hea和tabs-body，但你没有写子组件')
+      }
       if (vm.$options.name === 'gTabsHead') {
         vm.$children.forEach((childVm) => {
           //childVm.name是childVm里props里的name
