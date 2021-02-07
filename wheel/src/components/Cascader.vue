13fb55e185fd3ce2973dc92e047bd3494772aac5
way1234zzZ <2979523100@qq.com>
2021-02-08 01:41:05 +0800

    完成基本样式

--- wheel/src/components/Cascader.vue
+++ wheel/src/components/Cascader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cascader">
     <div class="trigger" @click="popoverVisible = !popoverVisible">
-      <slot></slot>
+      {{result || '请选择'}}
     </div>
     <div class="popOver" v-if="popoverVisible">
       <g-cascader-item :items="source" :height="popoverHeight" :selected="selected" @update:selected="onUpdateSelected"></g-cascader-item>
@@ -32,6 +32,9 @@ export default {
     }
   },
   computed: {
+    result() {
+      return this.selected.map((item) => item.name).join('/')
+    }
   },
   components: {
     gCascaderItem
@@ -47,13 +50,18 @@ export default {
 .cascader {
   position: relative;
   .trigger {
-    border: 1px solid black;
+    border: 1px solid #ddd;
     height: 32px;
-    width: 100px;
+    min-width: 10em;
+    display: inline-flex;
+    align-items: center;
+    padding: 0 1em;
+    border-radius: 4px;
   }
   .popOver {
     position: absolute;
     top: 100%;
+    margin-top: 4px;
     //弹出窗加颜色盖住原本的字体 不然弹出窗的内容与本来的重叠
     background: white;
     left: 0;
