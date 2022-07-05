b615b640f10e02fdf669dac1af6843bd4ba55fbd
way1234zzZ <2979523100@qq.com>
2022-07-05 22:34:09 +0800

    xiala

---
+++ wheel/src/components/xiala.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrapper">
+    <div class="button">
+      点击
+    </div>
+    <div class="content" v-if="show">
+      <span v-for="(item,index) in lists" :key="index + item"></span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      lists: []
+    }
+  }
+}
+</script>
+<style >
+.wrapper {
+  display: flex;
+}
+</style>
